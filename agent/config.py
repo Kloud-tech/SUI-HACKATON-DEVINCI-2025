4b8da72811c0fdb137e11ee90a729ea3b38f3345
Romain <romainbernardmassias50@gmail.com>
2025-11-22 15:11:13 +0100

    Ajout config et agent_step0

---
+++ agent/config.py
@@ -0,0 +1,15 @@
+# config.py
+
+# URL RPC Sui (à adapter selon votre réseau : devnet/testnet/mainnet local)
+SUI_RPC_URL = "https://fullnode.testnet.sui.io:443"  # exemple testnet, change si besoin
+
+# ID de l'objet Chimera à analyser (Arch te le donnera après le déploiement)
+CHIMERA_ID = "0x123456789abcdef..."  # TODO: remplacer par le vrai ID
+
+# Indices dans le vecteur DNA (si dna = [STR, AGI, INT])
+DNA_INDEX_STR = 0
+DNA_INDEX_AGI = 1
+DNA_INDEX_INT = 2
+
+# Objectif génétique pour la démo
+TARGET_STR = 50
